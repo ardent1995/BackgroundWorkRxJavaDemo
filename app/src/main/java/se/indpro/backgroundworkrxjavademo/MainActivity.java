7d8fd024e5722963f2ceba9133d0b05d12b96adf
ardent1995 <ikbaltohosif321@gmail.com>
2019-01-31 14:54:38 +0530

    Project Setup

---
+++ app/src/main/java/se/indpro/backgroundworkrxjavademo/MainActivity.java
@@ -0,0 +1,32 @@
+package se.indpro.backgroundworkrxjavademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        recyclerView = findViewById(R.id.rv_logs);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonClicked();
+            }
+        });
+    }
+
+    private void buttonClicked(){
+        Toast.makeText(this,"Button Clicked",Toast.LENGTH_LONG).show();
+    }
+}
